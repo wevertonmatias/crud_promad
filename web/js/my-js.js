3721186c1ce6be798a8fe988ed1a4e9fc5266593
Weverton Matias <weverton-matias@hotmail.com>
2021-12-18 22:35:10 -0300

    Realizado alguns ajustes no front-end e inicializados os assets my-js, jquery e jquery-mask.

---
+++ web/js/my-js.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("#loginform-username").inputmask("999.999.999-99", {removeMaskOnSubmit: true})
+    $("#signupform-username").inputmask("999.999.999-99", {removeMaskOnSubmit: true})
+    $("#user-username").inputmask("999.999.999-99", {removeMaskOnSubmit: true})
+});
